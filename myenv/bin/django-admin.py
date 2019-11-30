79ea86d9fb508e22655ba56fb2c03e5752d49c8c
Raiyan <raiyan.sharif.1234@gmail.com>
2019-11-30 17:34:19 +0600

    virtualenv correction

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/raiyansharif/Documents/DU MIT/MY/1stSemester/pythonProject/Django-Banking-App-master/myenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
